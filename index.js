ee4d449433d7a2f4f983fc1b6dfe98da7b9fc105
SA Al Walid <saawalid03@gmail.com>
2025-05-29 14:29:40 +0600

    fix:render deployment fix

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://task-goals-client-nfzz.vercel.app",
+    ],
 
     credentials: true,
   })
